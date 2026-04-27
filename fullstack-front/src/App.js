c54e5eb8ca93253efe8fb300b076a2d816e7ed5a
Rishikesh Pandey <rishipandey007@yahoo.com>
2026-04-27 11:39:47 +0530

    Initial commit

---
+++ fullstack-front/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import AddUser from './users/AddUser';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EditUser from './users/Edituser';
+import ViewUser from './users/ViewUser';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>      
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+          <Route exact path='/edituser/:id' element={<EditUser/>}/>
+           <Route path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
